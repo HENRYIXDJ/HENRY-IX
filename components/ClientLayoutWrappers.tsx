384316ff49987f82bcce07ef14e74bedc0aad2aa
henryixdj-art <henryixdj@gmail.com>
2026-05-22 06:58:45 +0100

    feat: restrict sensory warning preloader to initial landing on home route only

--- components/ClientLayoutWrappers.tsx
+++ components/ClientLayoutWrappers.tsx
@@ -1,13 +1,28 @@
 'use client';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 import { Preloader, CRTOverlay } from '@/components/DJComponents';
 import SiteHeader from '@/components/SiteHeader';
 import { useAudioStore } from '@/store/audioStore';
 
 export default function ClientLayoutWrappers() {
+  const pathname = usePathname();
+  const preloaderComplete = useAudioStore(s => s.preloaderComplete);
   const setPreloaderComplete = useAudioStore(s => s.setPreloaderComplete);
+
+  useEffect(() => {
+    if (pathname !== '/' && !preloaderComplete) {
+      setPreloaderComplete(true);
+    }
+  }, [pathname, preloaderComplete, setPreloaderComplete]);
+
+  const showPreloader = pathname === '/' && !preloaderComplete;
+
   return (
     <>
-      <Preloader onComplete={() => setPreloaderComplete(true)} />
+      {showPreloader && (
+        <Preloader onComplete={() => setPreloaderComplete(true)} />
+      )}
       <CRTOverlay />
       <SiteHeader />
     </>
